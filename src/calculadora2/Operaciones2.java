9b8611df4fbb3ef9ea82fae4d538b459fd0724c6
Estudiante <Estudiante@172.16.10.177>
2019-05-08 20:02:31 -0500

    taller calculadora

---
+++ src/calculadora2/Operaciones2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Operaciones2 {
+    
+     public static double sumar(double n1, double n2) {
+       return n1 + n2;
+    }
+
+    public static double restar(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public static double dividir(double n1, double n2) {
+        return n1 / n2;
+    }
+
+}
+
+    
+
